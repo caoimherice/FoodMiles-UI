bee98d681009aa0a03b892feac1b4bc7a9d65d55
caoimherice <c.rice10@nuigalway.ie>
2023-01-15 16:42:38 +0000

    adding angular cognito

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LogInComponent } from './components/log-in/log-in.component';
-import { RegisterComponent } from './components/register/register.component';
+//import { LogInComponent } from './components/log-in/log-in.component';
+//import { RegisterComponent } from './components/register/register.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { ProfileComponent } from './components/profile/profile.component';
+// const routes: Routes = [
+//   { path: '', pathMatch: 'full', redirectTo: 'login' },
+//   { path: 'login', component: LogInComponent },
+//   { path: 'register', component: RegisterComponent },
+//   { path: 'signup', component: SignUpComponent }
+// ];
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: 'login', component: LogInComponent },
-  { path: 'register', component: RegisterComponent }
+  {
+    path: '',
+    redirectTo: 'signIn',
+    pathMatch: 'full',
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'signIn',
+    component: SignInComponent,
+  },
+  {
+    path: 'signUp',
+    component: SignUpComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'signIn',
+  },
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,47 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+// import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { LogInComponent } from './components/log-in/log-in.component';
+// import { RegisterComponent } from './components/register/register.component';
+// import { SignUpComponent } from './components/sign-up/sign-up.component';
+// import { AngularMaterialModule } from './angular-material.module';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+// import { SignInComponent } from './components/sign-in/sign-in.component';
+// import { ProfileComponent } from './components/profile/profile.component';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LogInComponent } from './components/log-in/log-in.component';
-import { RegisterComponent } from './components/register/register.component';
-import { AngularMaterialModule } from './angular-material.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogInComponent,
-    RegisterComponent
+    //LogInComponent,
+    //RegisterComponent,
+    SignUpComponent,
+    SignInComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    AngularMaterialModule,
+    //HttpClientModule,
+    //BrowserAnimationsModule,
+    //AngularMaterialModule,
     FormsModule,
-    ReactiveFormsModule,
+    //ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  //schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
