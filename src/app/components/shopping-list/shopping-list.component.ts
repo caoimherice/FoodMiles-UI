1872f432590b79db30cbf4b2b3ffdbc26bb8195e
caoimherice <c.rice10@nuigalway.ie>
2023-02-11 16:13:30 +0000

    display shopping list items for a user, need to make it look pretty

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {CognitoService} from "../../cognito.service";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit{
+  items: any[] = [];
+  constructor(private router: Router, private http: HttpClient, private route: ActivatedRoute, private cognitoService: CognitoService) {
+  }
 
+  ngOnInit() {
+    this.cognitoService.getSession()
+      .then(session => {
+        const headers = new HttpHeaders().set('Authorization', 'Bearer ' + session.getIdToken().getJwtToken());
+        console.log(headers)
+        this.http
+          .get('https://gjru6axeok.execute-api.us-east-1.amazonaws.com/shoppingList/details/'+ session.getIdToken().payload['cognito:username'], {headers})
+          .subscribe((response: Object) => {
+            this.items = response as any[];
+          });
+      });
+  }
 }
