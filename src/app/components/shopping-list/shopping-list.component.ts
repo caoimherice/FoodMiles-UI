90c6e6b3ba1ce4382652e053a81c4f4f9d293b53
caoimherice <c.rice10@nuigalway.ie>
2023-02-20 20:06:40 +0000

    save shopping lists for a user

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -25,4 +25,23 @@ export class ShoppingListComponent implements OnInit{
           });
       });
   }
+  saveList() {
+    var url = 'https://gjru6axeok.execute-api.us-east-1.amazonaws.com/savedList/list'
+    // let headers;
+    this.cognitoService.getSession()
+      .then(session => {
+        var data = {
+          'userId': session.getIdToken().payload['cognito:username'],
+          'items': this.items
+        }
+        const headers = new HttpHeaders().set('Authorization', 'Bearer ' + session.getIdToken().getJwtToken());
+        console.log(headers)
+        this.http
+          .post(url, data,{headers})
+          .subscribe((response) => {
+            // this.router.navigate(['shoppingList']);
+            console.log(response)
+          });
+      });
+  }
 }
