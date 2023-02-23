defc611a198219c00c48e612be319ae38d2d355e
caoimherice <c.rice10@nuigalway.ie>
2023-02-23 21:20:44 +0000

    updating map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -15,19 +15,45 @@ export class MapComponent implements OnInit {
   map: L.Map | undefined;
 
   ngOnInit() {
-    this.map = L.map('map').setView([51.505, -0.09], 13);
+    this.map = L.map('map').setView([35.8617, 104.1954], 3);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors'
     }).addTo(this.map);
 
+    const boatIcon = L.icon({
+      iconUrl: "../../assets/boat-icon.png",
+      iconSize: [32, 32], // Size of the icon image
+    });
+
+    const boatMarker = L.marker([35.8617, 104.1954], {
+      icon: boatIcon,
+    }).addTo(this.map);
+
     const routingControl = (<any>L).Routing.control({
       waypoints: [
-        L.latLng(51.5, -0.1),
-        L.latLng(51.51, -0.12)
+        // L.latLng(35.8617, 104.1954), // China,
+        // L.latLng(40.416775, -3.703790), // Madrid, Spain
+        // L.latLng(53.2738, -9.0518) // Galway, Ireland
+        // L.latLng(	38.946594, 121.608353), // Port of Origin
+        // L.latLng(	41.346176, 2.168365) // Port of Destination
+        L.latLng(13.65844, 100.54333), // China,
+        L.latLng(9.077010, 101.735355), // China,
+        L.latLng(1.269957, 103.796127), // China,
       ],
-      routeWhileDragging: true
-    });
-    routingControl.addTo(this.map);
+      routeWhileDragging: true,
+      // router: new (<any>L).Routing.osrmv1({
+      //   serviceUrl: 'http://router.project-osrm.org/route/v1',
+      // }),
+      lineOptions: {
+        styles: [
+          {color: '#00BFFF', weight: 6}, // Blue line with a weight of 6 pixels
+        ]
+      }
+    }); routingControl.addTo(this.map)
+    // const latlngs = [[38.91,-77.07], [37.77, -79.43], [39.04, -85.2]];
+    // var polyline = L.polyline([[38.91,-77.07], [37.77, -79.43]], {color: 'red'});
+    // polyline.addTo(this.map)
+
   }
 }
